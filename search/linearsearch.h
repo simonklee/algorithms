59820088a4d056e8cdb8c1827f688089054cae48
Simon Zimmermann <simonz05@gmail.com>
2010-01-05 15:46:22 +0100

    header file for lsearch

---
+++ search/linearsearch.h
@@ -0,0 +1,9 @@
+#ifndef __LINEARSEARCH_H
+#define __LINEARSEARCH_H
+
+int lfind(int *key, int *base, int n); 
+void linsert(int *key, int *base, int *n, int max);
+void lsortinsert(int *key, int *base, int *n, int max);
+void ldelete(int *key, int *base, int *n);
+
+#endif
