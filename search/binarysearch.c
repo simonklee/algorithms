b51a6d8e23c24381d832614d141bed86923083cc
Simon Zimmermann <simonz05@gmail.com>
2010-01-08 19:16:07 +0100

    minor corrections

--- search/binarysearch.c
+++ search/binarysearch.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
+static void shiftLeft(int *base, int n, int l);
+static void shiftRight(int *base, int n, int l);
+
+// dichotomic search algorithm with 0(log n) effiency
 int BinarySearch(int *key, int *base, int n) {
     int min = 0, max = n, mid;
     
-    while (min < max) {
+    while (min <= max) {
         mid = (min + max) / 2;
         if (base[mid] < *key)
             min = mid + 1;
         else if (base[mid] > *key)
-            max = mid;
+            max = mid - 1;
         //else if (base[mid] == *key) 
         // if not bigger or not smaller, then === key
         else 
@@ -22,7 +26,7 @@ int BinarySearch(int *key, int *base, int n) {
 int BinarySearch2(int *key, int *base, int min, int max) {
     if (max < min)
         return -1;
-    int mid = min + ((max - min) / 2);
+    int mid = (min + max)  / 2;
     if (base[mid] > *key)
         return BinarySearch2(key, base, min, mid - 1);
     else if (base[mid] < *key)
@@ -32,3 +36,5 @@ int BinarySearch2(int *key, int *base, int min, int max) {
 }
 void BinaryDelete(int *key, int *base, int *n) {}
 void BinaryInsert(int *key, int *base, int *n, int max) {}
+
+
