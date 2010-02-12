3f1dc319e186eb783dddb8f3c63bfd21a9a88202
Simon Zimmermann <simonz05@gmail.com>
2010-02-12 11:23:26 +0100

    Added a blueJ projected used to create a sorted Insertion List

---
+++ misc/o1/SortedList.java
@@ -0,0 +1,28 @@
+
+public class SortedList<T> {
+    private List<? super T> list;
+    private Comperator<Object> comp;
+
+    public SortedList(List<T> list, Comperator<Object> comp) {
+        this.list = list;
+        this.comp = comp;
+    }
+    
+    public List Sort() {
+        Node mark;
+        Node reverse;
+        Node current = list.First().Next();
+        while (current != null) {
+            mark = current;
+            reverse = current;
+            while (reverse.Prev() != null && comp.compare(reverse.Prev().Data(), mark.Data()) > 0) {
+                reverse = reverse.Prev();
+            }
+            if (mark != reverse) {
+                list.Move(mark, reverse);
+            }
+            current = current.Next();
+        }
+        return list;
+    }
+}
