7cd2066aca23f389fbb04b1e573084efe59a044b
Simon Zimmermann <simonz05@gmail.com>
2010-02-14 19:09:51 +0100

    only works for real numbers

--- recursion/pow/pow.go
+++ recursion/pow/pow.go
@@ -4,31 +4,19 @@ import (
     "fmt"
 )
 
-func pow2(x, y int) (int) {
-    if y == 0 {
-        fmt.Printf("Base reached, returning x(%d)\n", x)
-        return 1
-    }
-//  fmt.Printf("%d * %d == ", x, x)
-//  fmt.Printf("%d\n", x)
-    return (pow(x, y - 1) * x)
-}
-
 func pow(x, y int) (int) {
-    if y == 1 {
-//      fmt.Printf("Base reached, returning x(%d)\n", x)
-        return x 
+    if y <= 1 {
+        fmt.Printf("Base reached, returning x(%d)\n", x)
+        return x
     }
-//  fmt.Printf("%d * %d == ", x, x)
+    fmt.Printf("%d * %d == ", x, x)
     x *= x
-//  fmt.Printf("%d\n", x)
+    fmt.Printf("%d\n", x)
     return pow(x, y/2)
 }
 
 func main() {
     x := 2
-    y := 8
-    fmt.Printf("pow2(%d, %x) == %d\n", x, y, pow2(x, y))
-    fmt.Printf("pow(%d, %x) == %d\n", x, y, pow(x, y))
- 
+    y := 4
+    fmt.Printf("pow(%d, %d) == %d\n", x, y, pow(x, y))
 }
