b0f8c88c4e7793b6d0457378f8c06f208ca9a128
Simon Zimmermann <simonz05@gmail.com>
2010-02-14 18:36:28 +0100

    cleaning

--- misc/o1/Comperator.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * A simple comperator interface
- */
-
-public interface Comperator<T>
-{
-    int compare(T o1, T o2);
-}
--- misc/o1/Sort.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Sorting.
- */
-public abstract class Sort
-{
-    public abstract int Swap(int a, int b);
-    public abstract int Compare();
-}
