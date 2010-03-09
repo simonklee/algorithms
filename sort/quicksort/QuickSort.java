1295b6d0ce6d5ef98bbecdb83a3d735f2e81f03e
Simon Zimmermann <simonz05@gmail.com>
2010-03-09 18:44:46 +0100

    quicksort mostly ok i think

--- sort/quicksort/QuickSort.java
+++ sort/quicksort/QuickSort.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 /**
  * @author  Simon Zimmermann
  */
@@ -7,9 +7,7 @@ public class QuickSort
     private int[] array;
     private int n;
 
-    public QuickSort(int[] array) {
-        this.array = array;
-        this.n = array.length;
+    public QuickSort() {
 
     }
 
@@ -17,29 +15,42 @@ public class QuickSort
      * Do Quicksort on array.
      */
     public void sort() {
-        qsort(0, n);
+        qsort(0, n-1);
     }
     
-    private void qsort(int left, int right) {}
+    private void qsort(int left, int right) {
+        // since it's called recursivly we need to make it stop.
+        if (right - left < 1)
+            return;
+        else {
+            // we use the rightmost elem as pivot.
+            int piv = array[right];
+            
+            int part = partition(left, right, piv); 
+            qsort(left, part - 1);
+            qsort(part + 1, right);
+        }
+    }
    
     /**
      * Partition will move items bigger then piv from left to 
      * the right end of the array, replacing smaller items
      * on the right, in a swap. 
      */
-    public int partition(int left, int right, int piv) {
-        while (true) {
-            while (left < right) {
-                if (array[left] > piv) 
-                    break;
+    public int partition(int leftPtr, int rightPtr, int piv) {
+        int left = leftPtr;
+        int right = rightPtr;
+        while (left <= right) {
+            while (array[left] < piv) {
                 left++;
             }
-            while (left < right && array[--right] > piv);
+            while (array[--right] > piv);
     
             if (left >= right)
                 break;
             swap(left, right);
         }
+        swap(left, rightPtr);
         return left;
     }    
 
@@ -75,10 +86,33 @@ public class QuickSort
      * Print array.
      */
     public void print() {
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < array.length; i++)
             System.out.print(array[i] + " ");
         System.out.println();
     }
+    
+    public void fill() {
+        int N = 10;    
+        array = new int[N];
+        Random rand = new Random();
+
+        for (int i = 0; i < N; i++) {
+            int nr = rand.nextInt(99);
+            while (find(array, nr) > 0) {
+                nr = rand.nextInt(99);
+            }
+            array[i] = nr;
+            this.n = array.length;
+        }
+    }
+    
+    private int find(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) 
+            if (arr[i] == key)
+                return i;
+        return -1;
+    }  
+    
     
     // Get and set methods.
     public int getN() {return n;}
--- sort/quicksort/TestQuickSort.java
+++ sort/quicksort/TestQuickSort.java
@@ -1,3 +1,4 @@
+
 /**
  * The test class TestQuickSort.
  */
@@ -5,20 +6,32 @@
 public class TestQuickSort extends junit.framework.TestCase
 {
     private QuickSort quick;
-    
     static int[] arrDefault = {3, 8, 9, 10, 2, 1, 4, 5, 7, 6};
+    private int[] arrBig = {13, 65, 95, 21, 47, 79, 58, 39, 45, 55};
+    private int[] arrBigSorted = {13, 21, 39, 45, 47, 55, 58, 65, 79, 95};
     
     public TestQuickSort()  {
-        quick = new QuickSort(arrDefault);
+        quick = new QuickSort();
+
+
     }
-    protected void setUp() { }
-    protected void tearDown() { }
+    
+
+    
+    protected void setUp() {}
+    protected void tearDown() {}
     
     public void testDefaultSort() {testSort(arrDefault); }
+    public void testBigSort() {
+        quick.setArray(arrBig);
+        quick.sort();
+        
+        for (int i = 0; i < quick.getN(); i++) {
+            assertEquals(arrBigSorted[i], quick.getArray()[i]);
+        }
+        
+    }
     
-    /**
-    * Private array test method.
-    */
 
     /**
      * Private array test method.
