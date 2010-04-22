ffec3772826c4eff43d50eb9cea6e6f6fb59b9ed
Simon Zimmermann <simonz05@gmail.com>
2010-04-22 22:52:29 +0200

    Create the outline project, pep8ify and such

--- misc/o2/src/project.py
+++ misc/o2/src/project.py
@@ -5,49 +5,49 @@ import re
 
 
 class Project(object):
-    
+
     def __init__(self, filename):
         self.tasks = dict()
         self._load(filename)
 
     def _load(self, filename):
         '''
-        Load tasks from filename and store them in the 
+        Load tasks from filename and store them in the
         Tasks dictionary.
-        
-        '''
-        fp = open(filename, mode='r')
-        lines = (line.strip().split(' ') for line in fp.readlines()
-            if re.search(r'^\w', line))
-        fp.close()
-        
-        for line in lines:
-            print "Task: %s duration: %s preds: " % (line[0], line[1]),
-            for i, l in enumerate(line[3:]):
-                print "%s" % l,
-            print ""
-        
 
-    def _task(self, name):
-        '''
-        Create a task for given name if it doesn't exists.
-        Returns the task for the given name.
+        The tasks are read from file specified. Only lines who start with a
+        word will be recognized. the tasks are split by whitespace in the file,
+        and a set of predecessors(dependencies) are listed after a colon, those
+        are also split by whitespace.
 
         '''
-        if name not in self.tasks:
-            tasks[name] = Task(name)
-        return tasks[name]
-        
+        with open(filename, mode='r') as fp:
+            lines = (line.strip().split(' ') for line in fp.readlines()
+                if re.search(r'^\w', line))
+
+        try:
+            for line in lines:
+                name, duration = line[0], line[1]
+                predecessors = [self.tasks[p] for p in line[3:]]
+                self.tasks[name] = Task(name, duration, predecessors)
+
+                print "Task: %s duration: %s preds: " % (name, duration),
+                for l in predecessors:
+                    print "%s" % l.name,
+                print ""
+        except IndexError:
+            print 'Failed to desipher the tasks-file. Please check your syntax'
+            exit()
+
 
 class Task(object):
-    
-    def __init__(self, name):
+
+    def __init__(self, name, duration=None, predecessors=[], **kwargs):
         self.name = name
         self.predecessors = []
-
-    def set_duration(self, duration):
         self.duration = duration
+        self.earliest_start = kwargs.pop('earliest_start', None)
+        self.latest_start = kwargs.pop('latest_start', None)
 
 if __name__ == '__main__':
     project = Project('testproject.txt')
-    
