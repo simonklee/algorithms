7466871dcfd5ff39f7eb2930f5c422a6ce0676ca
Simon Zimmermann <simonz05@gmail.com>
2010-04-26 15:49:45 +0200

    small comment fix

--- misc/o2/src/test_project.py
+++ misc/o2/src/test_project.py
@@ -6,9 +6,9 @@ from project import Project, Task
 
 
 class ProjectTest(unittest.TestCase):
-    
+
     def __init__(self, *args, **kwargs):
-        self.p = Project('testproject.txt')    
+        self.p = Project('testproject.txt')
         super(ProjectTest, self).__init__(*args, **kwargs)
 
     def test_earliest(self):
@@ -28,7 +28,7 @@ class ProjectTest(unittest.TestCase):
             #print "%s @ earliest_start: %s" % (name, tree[name].earliest_start)
         #for k, v in tree.iteritems():
         #    print "%s @ %s" % (k, v.earliest_start)
-    
+
     def test_latest(self):
         expected = (
             ('START', 0),
@@ -52,46 +52,36 @@ class ProjectTest(unittest.TestCase):
     def test_critical(self):
         '''
         The critical path function will return one critical path.
-        
+
         This path is expected with the testproject.txt-file.
-        
+
         name : START, ea: 0, la: 0, dur: 0
-        name : A,     ea: 0, la: 0, dur: 3        
+        name : A,     ea: 0, la: 0, dur: 3
         name : D,     ea: 3, la: 3, dur: 1
         name : F,     ea: 4, la: 4, dur: 3
         name : END,   ea: 7, la: 7, dur: 0
-        
-        name : START, ea: 0, la: 0, dur: 0
-        name : B,     ea: 0, la: 0, dur: 4
-        name : C,     ea: 4, la: 4, dur: 3
-        name : END,   ea: 7, la: 7, dur: 0
-        
-        name : START, ea: 0, la: 0, dur: 0
-        name : A,     ea: 0, la: 0, dur: 3   
-        name : E,     ea: 3, la: 5, dur: 2
-        STOP
 
         '''
         expected = ('START', 'A', 'D', 'F', 'END')
-        
+
         p = Project('testproject.txt')
         p.earliest()
         p.latest()
         path = p.critical()
-        
+
         for i, t in enumerate(path):
             #print "name : %s, ea: %s, la: %s, dur: %s" % \
             #(t.name, t.earliest_start, t.latest_start, t.duration)
             self.assertEqual(t.name, expected[i])
-        
-        
+
+
 
     def test_topological(self):
         expected = ('START', 'A', 'B', 'C', 'E', 'D', 'F', 'END')
         p = Project('testproject.txt')
         p.earliest()
         p.latest()
-        
+
         for i, t in enumerate(p.topological()):
             #print "%s, %s" % (t.name, expected[i])
             self.assertEqual(t.name, expected[i])
@@ -102,53 +92,53 @@ class ProjectTest(unittest.TestCase):
         aaa = Task('A', 3)
         b = Task('B', 2)
         c = Task('C', 3)
-        
+
         self.assertTrue(a.__eq__(aa))
         self.assertTrue(aa.__eq__(a))
         self.assertTrue(aaa.__eq__(a))
         self.assertFalse(a.__eq__(b))
         self.assertFalse(b.__eq__(a))
-        
+
         se = set([a, b])
         self.assertTrue(a in se)
         self.assertTrue(aa in se)
         self.assertTrue(b in se)
         self.assertFalse(c in se)
-        
+
         c.earliest_start = 2
         a.earliest_start = 1
         self.assertFalse(a > c)
-  
+
 
 class Item(KeyedHashingMixin, KeyedComparisonMixin):
-    
+
     def __init__(self, x):
         self.x = x
-        
+
     def __key__(self):
         return self.x
 
 
 class TestPriorityQueue(unittest.TestCase):
-    
+
     items = [Item(x) for x in range(10)]
-    
+
     def test_init_pop(self):
         pq = PriorityQueue(lambda i: i.__key__(), self.items)
         self.assertEqual(pq.peek().x, 0)
         [self.assertEqual(pq.pop().x, i) for i in range(pq.size)]
         self.assertEqual(pq.size, 0)
-    
+
     def test_push_pop(self):
         pq = PriorityQueue(lambda i: i.__key__(), [])
         pq.push(Item(5))
         pq.push(Item(1))
         pq.push(Item(3))
         self.assertEqual(pq.pop().x, 1)
-    
+
     def test_contains(self):
         pq = PriorityQueue(lambda i: i.__key__(), self.items)
         self.assertEqual(pq.contains(Item(4)), True)
-    
+
 if __name__ == '__main__':
     unittest.main()
